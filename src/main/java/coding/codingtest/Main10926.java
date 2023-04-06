6bdab20a38379b8aceeab4f9f8c527d2e8035d9d
suminLee <pkpk85@naver.com>
2023-04-07 00:57:22 +0900

    first commit

---
+++ src/main/java/coding/codingtest/Main10926.java
@@ -0,0 +1,11 @@
+package coding.codingtest;
+import java.util.Scanner;
+
+public class Main10926 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String joonas = sc.nextLine();
+
+        System.out.println(joonas+"??!");
+    }
+}
