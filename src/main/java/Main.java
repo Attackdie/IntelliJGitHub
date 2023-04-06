57ffbec4913f3bf492d6449b6d3804c5684a0f0c
suminLee <pkpk85@naver.com>
2023-04-07 00:17:06 +0900

    test commit

---
+++ src/main/java/Main.java
@@ -0,0 +1,12 @@
+import java.util.ArrayList;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        String word = scanner.next(); // 입력받은 단어
+        ArrayList<String> list = new ArrayList<>();
+        list.add(word); // 리스트에 단어 추가
+        char lastChar = word.charAt(word.length() - 1); // 입력받은 단어의 마지막 글자
+        //
+    }
+}
