9ab9d50a7caa01412c30ae433e57c69aca5da947
suminLee <pkpk85@naver.com>
2023-04-08 21:25:56 +0900

    coding test commit

---
+++ src/main/java/coding/codingtest/study1/Main18108.java
@@ -0,0 +1,10 @@
+package coding.codingtest.study1;
+import java.util.Scanner;
+public class Main18108 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int year = sc.nextInt();
+
+        System.out.println(year - 543);
+    }
+}
