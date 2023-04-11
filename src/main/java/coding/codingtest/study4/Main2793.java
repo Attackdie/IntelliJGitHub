964f608436da03a63e16fc34e49861e481592198
suminLee <pkpk85@naver.com>
2023-04-11 21:03:22 +0900

    coding test commit5.0

---
+++ src/main/java/coding/codingtest/study4/Main2793.java
@@ -0,0 +1,12 @@
+package coding.codingtest.study4;
+import java.util.Scanner;
+public class Main2793 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+
+        for (int i = 1; i <= 9; i++) {
+            System.out.println(n + " * " + i + " = " + (n * i));
+        }
+    }
+}
