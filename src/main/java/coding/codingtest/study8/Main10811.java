b53c137b7060be1ddf657073bc5153f3c399f451
suminLee <pkpk85@naver.com>
2023-04-16 17:20:18 +0900

    coding test commit8.2

---
+++ src/main/java/coding/codingtest/study8/Main10811.java
@@ -0,0 +1,36 @@
+package coding.codingtest.study8;
+import java.util.*;
+
+public class Main10811 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int N = sc.nextInt();
+        int M = sc.nextInt();
+        int[] arr = new int[N];
+
+        // 배열 초기화
+        for (int i = 0; i < N; i++) {
+            arr[i] = i + 1;
+        }
+
+        // 뒤집기 연산 수행
+        for (int i = 0; i < M; i++) {
+            int start = sc.nextInt() - 1;
+            int end = sc.nextInt() - 1;
+
+            while (start < end) {
+                int temp = arr[start];
+                arr[start] = arr[end];
+                arr[end] = temp;
+                start++;
+                end--;
+            }
+        }
+
+        // 결과 출력
+        for (int i = 0; i < N; i++) {
+            System.out.print(arr[i] + " ");
+        }
+    }
+}
---
+++ src/main/java/coding/codingtest/study8/Main1546.java
@@ -0,0 +1,28 @@
+package coding.codingtest.study8;
+import java.util.*;
+
+public class Main1546 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int n = sc.nextInt();
+        double[] scores = new double[n];
+
+        double max = 0;
+        for (int i = 0; i < n; i++) {
+            scores[i] = sc.nextDouble();
+            if (scores[i] > max) {
+                max = scores[i];
+            }
+        }
+
+        double sum = 0;
+        for (int i = 0; i < n; i++) {
+            scores[i] = scores[i] / max * 100;
+            sum += scores[i];
+        }
+
+        double avg = sum / n;
+        System.out.println(avg);
+    }
+}
